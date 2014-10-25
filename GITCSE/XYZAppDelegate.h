c5471397d37639b90f0ba0fa8617e8982ac2a620
claylynn <chen.ling@stonybrook.edu>
2014-10-25 10:35:09 -0400

    Initial Commit

---
+++ GITCSE/XYZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XYZAppDelegate.h
+//  GITCSE
+//
+//  Created by Chen on 10/25/14.
+//  Copyright (c) 2014 sbu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
